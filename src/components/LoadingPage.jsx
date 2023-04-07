e044d79aa7e8c051a0c14457cd6f3e79f9d7f3e3
Cristobal Jose Quilimaco Lopez <quilimacox1@gmail.com>
2023-04-07 16:42:27 -0500

    imformation o image

--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -4,7 +4,7 @@ const LoadingPage = () => {
   return (
 <div className='loadingPage'>
   <div>
-  <img className="bxs-sun" src="./public/luz-del-sol.png" alt="" />
+  <img className="bxs-sun" src="public/luz-del-sol.png" alt="image sun happy" />
   </div>
         
     <p className="textLoading">Loading...</p>
