1464c0dccca76c5a4f9166e269d7d6948c7a704f
Cristobal Jose Quilimaco Lopez <quilimacox1@gmail.com>
2023-04-07 16:50:42 -0500

    import image of loading

--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -1,10 +1,10 @@
-
+import Sun from '/public/luz-del-sol.png';
 
 const LoadingPage = () => {
   return (
 <div className='loadingPage'>
   <div>
-  <img className="bxs-sun" src="public/luz-del-sol.png" alt="image sun happy" />
+  <img className='bxs-sun' src={Sun} alt="Ejemplo" />
   </div>
         
     <p className="textLoading">Loading...</p>
