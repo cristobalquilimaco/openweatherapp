da2a2b3e71df37f6d722df5ce1deb3d4f4a75208
Cristobal Jose Quilimaco Lopez <quilimacox1@gmail.com>
2023-04-07 16:39:55 -0500

    move icon

--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -4,7 +4,7 @@ const LoadingPage = () => {
   return (
 <div className='loadingPage'>
   <div>
-  <img className="bxs-sun" src="./public/backgrounds/luz-del-sol.png" alt="" />
+  <img className="bxs-sun" src="./public/luz-del-sol.png" alt="" />
   </div>
         
     <p className="textLoading">Loading...</p>
