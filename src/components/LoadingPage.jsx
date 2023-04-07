d17ec5b319b7dbc02fb754414cc43692b80050a5
Cristobal Jose Quilimaco Lopez <quilimacox1@gmail.com>
2023-04-07 16:00:56 -0500

    add direction image

--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -3,7 +3,7 @@
 const LoadingPage = () => {
   return (
 <div className='loadingPage'>
-        <img className="bx-sun" src="./public/backgrounds/luz-del-sol.png" alt="" />
+        <img className="bx-sun" src="/public/backgrounds/luz-del-sol.png" alt="" />
     <p className="textLoading">Loading...</p>
     </div>
               
