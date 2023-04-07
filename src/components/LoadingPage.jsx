865fbae5f2000eec6e14812d9566c20c4581fbda
Cristobal <quilimacox1@gmail.com>
2023-04-07 09:12:34 -0500

    form center

--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -4,7 +4,7 @@ const LoadingPage = () => {
   return (
 <div className='loadingPage'>
         <img className="bx-sun" src="./public/backgrounds/luz-del-sol.png" alt="" />
-    <p className="textLoading">loading...</p>
+    <p className="textLoading">Loading...</p>
     </div>
               
 
